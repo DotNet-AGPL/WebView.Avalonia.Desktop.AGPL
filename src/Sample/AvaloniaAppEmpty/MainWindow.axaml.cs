eec034041e17b36eda05b3d2501a923c7a84a4da
Think <Thinkxuhao@outlook.com>
2026-01-23 18:24:01 +0800

    新增AvaloniaAppEmpty 作为参照

---
+++ src/Sample/AvaloniaAppEmpty/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaAppEmpty
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
